ea7c8f0195e546c003184b29f3ad6b1b82a7234c
Christopher-Johnson <chris.s.johnson4@gmail.com>
2022-04-20 13:09:25 -0600

    Cleaned up test.

--- test/alloc_buff.js
+++ test/alloc_buff.js
@@ -3,17 +3,12 @@
  * Author: Chris Johnson
  * Date: March 2020
  */
-const testCase = require('mocha').describe;
-const assertions = require('mocha').it;
 const assert = require('chai').assert;
 const equal = assert.equal;
-const strictEqual = assert.strictEqual;
-const deepEqual = require('deep-eql');
-const rewire = require('rewire');
 
 const curVer = process.versions;
 
-const allocBuffer = rewire('../allocate_buffer.js').allocBuffer;
+const allocBuffer = require('../allocate_buffer.js').allocBuffer;
 
 describe('allocBuff()', function() {
 	describe('of length 10', function() {
